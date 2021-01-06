99fc1394689761d887fc07c85d74227e4d7fab0e
Richard Yoon <jy392@cornell.edu>
2021-01-06 03:57:33 -0500

    cleaning up fast library

--- fast/compile.sh
+++ fast/compile.sh
@@ -1,15 +1,25 @@
-# #!/bin/bash
-# echo "Generating executable with optimization profile flags..."
-# g++ -O3 -fprofile-generate -Wall -Wpedantic -std=c++17 -march=native -flto -pipe -pthread main.cpp bigint.cpp normal_bigint.cpp -o bigint 
+#!/bin/bash
+echo "Removing old files..."
+rm -rf ./bigint 
+rm -rf ./bigint.gcda
+rm -rf ./main.gcda
+rm -rf ./main.gcda
+rm -rf ./normal_bigint.gcda
+rm -rf ./encoded
 
-# echo "Generating files required for optimization profile..."
-# time bigint e < one-liners | bigint d
+echo "Generating executable with optimization profile flags..."
+# g++ -O3 -fprofile-generate -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
+g++ -O3 -fprofile-generate -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
 
-# echo "Generating new executable that uses opt. profiles..."
-# g++ -O3 -fprofile-use -Wall -Wpedantic -std=c++17 -march=native -flto -pipe -pthread main.cpp bigint.cpp normal_bigint.cpp -o bigint
+echo "Generating files required for optimization profile..."
+time bigint e < one-liners | bigint d
+
+echo "Generating new executable that uses opt. profiles..."
+g++ -O3 -fprofile-use -fprofile-correction -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
 
 
 
-# g++ -O3 -Wall -Wpedantic -std=c++17 -march=native -flto -pipe -pthread main.cpp bigint.cpp normal_bigint.cpp -o bigint
+# # No Optimization Profile 
+# g++ -O3 -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
 
-g++ -O3 -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
+
