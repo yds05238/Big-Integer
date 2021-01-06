7fb02cd08b3e58db79e79e8b25dcc63fd8c0f0b4
Richard Yoon <jy392@cornell.edu>
2021-01-06 04:08:38 -0500

    fast library under 4 seconds

--- fast/compile.sh
+++ fast/compile.sh
@@ -6,12 +6,14 @@ rm -rf ./main.gcda
 rm -rf ./main.gcda
 rm -rf ./normal_bigint.gcda
 rm -rf ./encoded
+rm -rf ./gmon.out
 
 echo "Generating executable with optimization profile flags..."
 # g++ -O3 -fprofile-generate -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
 g++ -O3 -fprofile-generate -Wall -Wpedantic -std=c++17 -march=native -flto -pipe main.cpp bigint.cpp normal_bigint.cpp -o bigint -lpthread
 
 echo "Generating files required for optimization profile..."
+clear
 time bigint e < one-liners | bigint d
 
 echo "Generating new executable that uses opt. profiles..."
